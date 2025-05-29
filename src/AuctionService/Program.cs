082cb39ef1899f2469dce8589f49212131752a12
user <user@users-MacBook-Pro.local>
2025-05-29 02:52:48 -0500

    Created CreateAuction Endpoint and GetAuctionById Endpoints

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -11,11 +11,28 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
 
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
 
 
 app.Run();
